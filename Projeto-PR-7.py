94fbc19c7ef68711f1df10ccf39d5ecd291b389f
Geraldo Galvao <gmgn@cin.ufpe.br>
2020-10-08 00:44:18 -0300

    Add keys

--- Projeto-PR-7.py
+++ Projeto-PR-7.py
@@ -4,6 +4,26 @@ from random import *
 from math import *
 
 Play.setInstrument(ROCK_ORGAN)
+
+keys = dict([
+    (VK_Q, (C4, 0)),
+    (VK_2, (CS4, 1)),
+    (VK_W, (D4, 2)),
+    (VK_3, (DS4, 3)),
+    (VK_E, (E4, 4)),
+    (VK_R, (F4, 5)),
+    (VK_5, (FS4, 6)),
+    (VK_T, (G4, 7)),
+    (VK_6, (GS4, 8)),
+    (VK_Y, (A4, 9)),
+    (VK_7, (AS4, 10)),
+    (VK_U, (B4, 11)),
+    (VK_I, (C5, 12)),
+    (VK_9, (CS5, 13)),
+    (VK_O, (D5, 14)),
+    (VK_0, (DS5, 15)),
+    (VK_P, (E5, 16))
+])
       
 class SynthWaveSynth:
    def __init__(self, width):
@@ -21,7 +41,27 @@ class SynthWaveSynth:
       self.polygons = []
       ## Images
       self.background = Icon("background.jpg", self.width, self.height / 2)
-      self.add_to_display()
+      
+      self.key_icons = [
+         Icon("keys/q.png", 25, 25),
+         Icon("keys/2.png", 25, 25),
+         Icon("keys/w.png", 25, 25),
+         Icon("keys/3.png", 25, 25),
+         Icon("keys/e.png", 25, 25),
+         Icon("keys/r.png", 25, 25),
+         Icon("keys/5.png", 25, 25),
+         Icon("keys/t.png", 25, 25),
+         Icon("keys/6.png", 25, 25),
+         Icon("keys/y.png", 25, 25),
+         Icon("keys/7.png", 25, 25),
+         Icon("keys/u.png", 25, 25),
+         Icon("keys/i.png", 25, 25),
+         Icon("keys/9.png", 25, 25),
+         Icon("keys/o.png", 25, 25),
+         Icon("keys/0.png", 25, 25),
+         Icon("keys/p.png", 25, 25),
+      ]
+      self.add_to_display(self.key_icons)
       
       self.keysPressed = []
       
@@ -39,12 +79,14 @@ class SynthWaveSynth:
          ys.append(self.height - y)
          last_y = y
          distance = (self.height / 2.0 - last_y) / 5.0
+      
       for line_index in range(len(self.horizontal_lines)):
          self.display.move(self.horizontal_lines[line_index], 0, int(ys[line_index]) - 5)
+      
       if ys[1] >= self.height:
          self.y_offset = 0.0
 
-   def add_to_display(self):
+   def add_to_display(self, key_icons):
       # Lines
       ## Horizontal lines
       line_color = Color(165, 96, 190)
@@ -80,148 +122,36 @@ class SynthWaveSynth:
          self.display.add(line)
       
       self.display.add(self.background)
+      
+      for x in range(1, keys + 1):
+         self.display.add(
+             key_icons[x - 1], 
+             (x * (self.width / (keys + 1))) - 12, 
+             self.height - 50)
+         
 
    def beginNote(self, key):
       print "Key pressed is " + str(key)
- 
-      if key == VK_Q and key not in self.keysPressed:
-         Play.noteOn( C4 )
-         self.keysPressed.append( VK_Q )
-         self.polygons[0].setColor(Color(255, 255, 255, 70))
-      elif key == VK_2 and key not in self.keysPressed:
-         Play.noteOn( CS4 )
-         self.keysPressed.append( VK_2 )
-         self.polygons[1].setColor(Color(255, 255, 255, 70))
-      elif key == VK_W and key not in self.keysPressed:
-         Play.noteOn( D4 )
-         self.keysPressed.append( VK_W )
-         self.polygons[2].setColor(Color(255, 255, 255, 70))
-      elif key == VK_3 and key not in self.keysPressed:
-         Play.noteOn( DS4 )
-         self.keysPressed.append( VK_3 )
-         self.polygons[3].setColor(Color(255, 255, 255, 70))
-      elif key == VK_E and key not in self.keysPressed:
-         Play.noteOn( E4 )
-         self.keysPressed.append( VK_E )
-         self.polygons[4].setColor(Color(255, 255, 255, 70))
-      elif key == VK_R and key not in self.keysPressed:
-         Play.noteOn( F4 )
-         self.keysPressed.append( VK_R )
-         self.polygons[5].setColor(Color(255, 255, 255, 70))
-      elif key == VK_5 and key not in self.keysPressed:
-         Play.noteOn( FS4 )
-         self.keysPressed.append( VK_5 )
-         self.polygons[6].setColor(Color(255, 255, 255, 70))
-      elif key == VK_T and key not in self.keysPressed:
-         Play.noteOn( G4 )
-         self.keysPressed.append( VK_T )
-         self.polygons[7].setColor(Color(255, 255, 255, 70))
-      elif key == VK_6 and key not in self.keysPressed:
-         Play.noteOn( GS4 )
-         self.keysPressed.append( VK_6 )
-         self.polygons[8].setColor(Color(255, 255, 255, 70))
-      elif key == VK_Y and key not in self.keysPressed:
-         Play.noteOn( A4 )
-         self.keysPressed.append( VK_Y )
-         self.polygons[9].setColor(Color(255, 255, 255, 70))
-      elif key == VK_7 and key not in self.keysPressed:
-         Play.noteOn( AS4 )
-         self.keysPressed.append( VK_7 )
-         self.polygons[10].setColor(Color(255, 255, 255, 70))
-      elif key == VK_U and key not in self.keysPressed:
-         Play.noteOn( B4 )
-         self.keysPressed.append( VK_U )
-         self.polygons[11].setColor(Color(255, 255, 255, 70))
-      elif key == VK_I and key not in self.keysPressed:
-         Play.noteOn( C5 )
-         self.keysPressed.append( VK_I )
-         self.polygons[12].setColor(Color(255, 255, 255, 70))
-      elif key == VK_9 and key not in self.keysPressed:
-         Play.noteOn( CS5 )
-         self.keysPressed.append( VK_9 )
-         self.polygons[13].setColor(Color(255, 255, 255, 70))
-      elif key == VK_O and key not in self.keysPressed:
-         Play.noteOn( D5 )
-         self.keysPressed.append( VK_O )
-         self.polygons[14].setColor(Color(255, 255, 255, 70))
-      elif key == VK_0 and key not in self.keysPressed:
-         Play.noteOn( DS5 )
-         self.keysPressed.append( VK_0 )
-         self.polygons[15].setColor(Color(255, 255, 255, 70))
-      elif key == VK_P and key not in self.keysPressed:
-         Play.noteOn( E5 )
-         self.keysPressed.append( VK_P )
-         self.polygons[16].setColor(Color(255, 255, 255, 70))  
+   
+      if key in keys:
+            Play.noteOn( keys[key][0] )
+            self.keysPressed.append( key )
+            self.polygons[keys[key][1]].setColor(Color(255, 255, 255, 70))
+            self.key_icons[keys[key][1]].setSize(50, 50)
+            self.display.move(
+                self.key_icons[keys[key][1]], 
+                ((keys[key][1] + 1) * (self.width / (len(keys) + 1))) - 25, 
+                self.height - 62)
  
    def endNote(self, key):
-      if key == VK_Q:
-         Play.noteOff( C4 )
-         self.keysPressed.remove( VK_Q )
-         self.polygons[0].setColor(Color(255, 255, 255, 0))
-      elif key == VK_2:
-         Play.noteOff( CS4 )
-         self.keysPressed.remove( VK_2 )
-         self.polygons[1].setColor(Color(255, 255, 255, 0))
-      elif key == VK_W:
-         Play.noteOff( D4 )
-         self.keysPressed.remove( VK_W )
-         self.polygons[2].setColor(Color(255, 255, 255, 0))
-      elif key == VK_3:
-         Play.noteOff( DS4 )
-         self.keysPressed.remove( VK_3 )
-         self.polygons[3].setColor(Color(255, 255, 255, 0))
-      elif key == VK_E:
-         Play.noteOff( E4 )
-         self.keysPressed.remove( VK_E )
-         self.polygons[4].setColor(Color(255, 255, 255, 0))
-      elif key == VK_R:
-         Play.noteOff( F4 )
-         self.keysPressed.remove( VK_R )
-         self.polygons[5].setColor(Color(255, 255, 255, 0))
-      elif key == VK_5:
-         Play.noteOff( FS4 )
-         self.keysPressed.remove( VK_5 )
-         self.polygons[6].setColor(Color(255, 255, 255, 0))
-      elif key == VK_T:
-         Play.noteOff( G4 )
-         self.keysPressed.remove( VK_T )
-         self.polygons[7].setColor(Color(255, 255, 255, 0))
-      if key == VK_6:
-         Play.noteOff( GS4 )
-         self.keysPressed.remove( VK_6 )
-         self.polygons[8].setColor(Color(255, 255, 255, 0))
-      elif key == VK_Y:
-         Play.noteOff( A4 )
-         self.keysPressed.remove( VK_Y )
-         self.polygons[9].setColor(Color(255, 255, 255, 0))
-      elif key == VK_7:
-         Play.noteOff( AS4 )
-         self.keysPressed.remove( VK_7 )
-         self.polygons[10].setColor(Color(255, 255, 255, 0))
-      elif key == VK_U:
-         Play.noteOff( B4 )
-         self.keysPressed.remove( VK_U )
-         self.polygons[11].setColor(Color(255, 255, 255, 0))
-      elif key == VK_I:
-         Play.noteOff( C5 )
-         self.keysPressed.remove( VK_I )
-         self.polygons[12].setColor(Color(255, 255, 255, 0))
-      elif key == VK_9:
-         Play.noteOff( CS5 )
-         self.keysPressed.remove( VK_9 )
-         self.polygons[13].setColor(Color(255, 255, 255, 0))
-      elif key == VK_O:
-         Play.noteOff( D5 )
-         self.keysPressed.remove( VK_O )
-         self.polygons[14].setColor(Color(255, 255, 255, 0))
-      elif key == VK_0:
-         Play.noteOff( DS5 )
-         self.keysPressed.remove( VK_0 )
-         self.polygons[15].setColor(Color(255, 255, 255, 0))
-      elif key == VK_P:
-         Play.noteOff( E5 )
-         self.keysPressed.remove( VK_P )
-         self.polygons[16].setColor(Color(255, 255, 255, 0))
+      if key in keys:
+            Play.noteOff( keys[key][0] )
+            filter(lambda a: a != key, self.keysPressed)
+            self.polygons[keys[key][1]].setColor(Color(255, 255, 255, 0))
+            self.key_icons[keys[key][1]].setSize(25, 25)
+            self.display.move(
+                self.key_icons[keys[key][1]], 
+                ((keys[key][1] + 1) * (self.width / (len(keys) + 1))) - 12, 
+                self.height - 50)
 
-
-synthwave = SynthWaveSynth(800)
+synthwave = SynthWaveSynth(1000)
